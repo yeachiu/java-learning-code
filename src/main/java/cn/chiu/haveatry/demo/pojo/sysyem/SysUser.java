1dcac0223c2b3bd76a9aa63310d57cb4162d147c
yeachiu <32219751+yeachiu@users.noreply.github.com>
2019-06-26 18:22:38 +0800

    Create SysUser.java

---
+++ src/main/java/cn/chiu/haveatry/demo/pojo/sysyem/SysUser.java
@@ -0,0 +1,29 @@
+package cn.chiu.haveatry.demo.pojo.system;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+/**
+ * Created by yeachiu on 2019/6/10.
+ */
+@Data
+public class SysUser implements Serializable  {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String id;
+    private String username;
+    private String password;
+    private Integer status;
+    private Integer age;
+    private String avatar;
+    private Date createDate;
+
+//////////////////////////////////////////////////////////////
+
+    private List<SysRole> roles;
+    private Integer authStatus;
+
+}
